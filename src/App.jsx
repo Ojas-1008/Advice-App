4a63039bb8575be50b23b7b075b080b219803623
Ojas Shrivastava <ojasshrivastava1008@gmail.com>
2026-04-07 12:51:10 +0530

    Implement fetch logic and add 'Get New Advice' button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      const adviceText = response.data.slip.advice;
+      setAdvice(adviceText);
+    } catch (error) {
+      console.log("Failed to fetch advice: ", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
-    <h1>Hello</h1>
+    <div>
+      <p>{advice}</p>
+      <button onClick={fetchAdvice} type='button'>Get a New Advice</button>
+    </div>
   );
 }
